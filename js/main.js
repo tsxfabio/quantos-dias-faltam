793e60fc5c5807b5d0baea5df3c10e5fb72956f2
Fábio - PC IRJ Sports <leardinifabio90@gmail.com>
2021-10-28 15:15:44 -0300

    registrando datas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 /*  */
 
 const botaoSubmit = document.querySelector('[data-calcular]')
-botaoSubmit.addEventListener('click', () => {
+
+
+const registraDatas = (evento) => {
+
+    evento.preventDefault()
 
     const dataInicial = document.querySelector('[data-inicial]')
     const valor1 = dataInicial.value
+
     const dataFinal = document.querySelector('[data-final]')
     const valor2 = dataFinal.value
 
-    console.log('As datas inseridas foram' + ' ' + valor1 + ' e ' + valor2);
-})
+    const dataInicialFormata = moment(valor1).format('DD/MM/YYYY')
+    const dataFinalFormatada = moment(valor2).format('DD/MM/YYYY')
+
+
+    console.log('As datas inseridas foram' + ' ' + dataInicialFormata + ' e ' + dataFinalFormatada)
+
+}
+
+/* const diffDatas = () => {
+
+    const dataFinal = document.querySelector('[data-final]')
+    valor = dataFinal.value
+    const prazo1 = moment(valor).subtract("30", "days") //Prazo para prorrogação do período de execução
+    prazoFinal = prazo1.value
+    console.log(prazoFinal)
+} */
+
+botaoSubmit.addEventListener('click', registraDatas)
+/* botaoSubmit.addEventListener('click', diffDatas) */
