a0e6c019fa00373c1c78d2f687e84e9b9f7249c7
Fabio Leardini <leardinifabio90@gmail.com>
2021-11-01 00:55:41 -0300

    att funcao diffDatas

--- js/main.js
+++ js/main.js
@@ -27,30 +27,37 @@ const diffDatas = () => {
     valor = dataFinal.value
 
     const prazo1 = moment(valor).subtract(30, 'days') //Prazo para prorrogação do período de execução;
-    console.log(prazo1.format('DD/MM/YYYY'))
+    const prazo1format = prazo1.format('DD/MM/YYYY')
 
     const prazo2 = moment(valor).subtract(60, 'days') //Prazo para solicitar readequação;
-    console.log(prazo2.format('DD/MM/YYYY'))
+    const prazo2format = prazo2.format('DD/MM/YYYY')
 
     const prazo3 = moment(valor).add(60, 'days') //Prazo para entregar a Prestação de Contas Final;
-    console.log(prazo3.format('DD/MM/YYYY'))
+    const prazo3format = prazo3.format('DD/MM/YYYY')
 
     const prazo4 = moment(valor).add(120, 'days') //Prazo para solicitar transferência de Saldo Remanescente;
-    console.log(prazo4.format('DD/MM/YYYY'))
+    const prazo4format = prazo4.format('DD/MM/YYYY')
 
-}
+    /* }
 
-const prazosList = () => {
-
+    const prazosList = (diffDatas) => { */
     const prazos = document.createElement('li')
 
     prazos.classList.add('prazos-conteudo')
 
-    const conteudo = `<p class="prazos-conteudo">Sua data limite para solicitar prorrogação do Período de Execução é ${prazo1}.</p>
-    <p class="prazos-conteudo"> Sua data limite para solicitar Readequação do Plano de Trabalho é ${prazo2}.</p>
-    <p class="prazos-conteudo"> Sua data limite para entregar a Prestação de Contas Final é ${prazo3}.</p>
-    <p class="prazos-conteudo"> Sua data limite para solicitar Transferência do Saldo Remanescente é ${prazo4}.</p>`
+    const conteudo = `<p class="prazos-conteudo">Sua data limite para solicitar prorrogação do Período de Execução é ${prazo1format}.</p>
+    <p class="prazos-conteudo"> Sua data limite para solicitar Readequação do Plano de Trabalho é ${prazo2format}.</p>
+    <p class="prazos-conteudo"> Sua data limite para entregar a Prestação de Contas Final é ${prazo3format}.</p>
+    <p class="prazos-conteudo"> Sua data limite para solicitar Transferência do Saldo Remanescente é ${prazo4format}.</p>`
+
+    prazos.innerHTML = conteudo
+
+    const listaPrazos = document.querySelector('[data-prazos-lista]')
+    listaPrazos.appendChild(prazos)
+
+    return prazos
 }
 
 botaoSubmit.addEventListener('click', registraDatas)
 botaoSubmit.addEventListener('click', diffDatas)
+botaoSubmit.addEventListener('click', prazosList)
