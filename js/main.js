c654b964fbb86796c51e032834a131e5fa1133f1
Fábio - PC IRJ Sports <leardinifabio90@gmail.com>
2021-11-19 14:52:59 -0300

    mainjs fix

--- js/main.js
+++ js/main.js
@@ -11,7 +11,10 @@ const registraDatas = (evento) => {
 export const diffDatas = () => {
 
     const dataFinal = document.querySelector('[data-final]')
-    valor = dataFinal.value
+    const dataInicial = document.querySelector('[data-inicial]')
+
+    valorInicial = dataInicial.value
+    valorFinal = dataFinal.value
 
     const prazo1 = moment(valor).subtract(30, 'days') //Prazo para prorrogação do período de execução;
     const prazo1format = prazo1.format('DD/MM/YYYY')
@@ -25,29 +28,32 @@ export const diffDatas = () => {
     const prazo4 = moment(valor).add(120, 'days') //Prazo para solicitar transferência de Saldo Remanescente;
     const prazo4format = prazo4.format('DD/MM/YYYY')
 
-    const printDatas = () => {
+    if (valorInicial > valorFinal) {
 
-        diffDatas() //entender o pq não importa as informações
+        const printDatas = () => {
 
-        const prazos = document.createElement('li')
+            diffDatas() //entender o pq não importa as informações
 
-        prazos.classList.add('prazos-conteudo')
+            const prazos = document.createElement('li')
 
-        const conteudo = `<p class="prazos-conteudo">Sua data limite para solicitar prorrogação do Período de Execução é ${prazo1format}.</p>
+            prazos.classList.add('prazos-conteudo')
+
+            const conteudo = `<p class="prazos-conteudo">Sua data limite para solicitar prorrogação do Período de Execução é ${prazo1format}.</p>
     <p class="prazos-conteudo"> Sua data limite para solicitar Readequação do Plano de Trabalho é ${prazo2format}.</p>
     <p class="prazos-conteudo"> Sua data limite para entregar a Prestação de Contas Final é ${prazo3format}.</p>
     <p class="prazos-conteudo"> Sua data limite para solicitar Transferência do Saldo Remanescente é ${prazo4format}.</p>`
 
-        prazos.innerHTML = conteudo
+            prazos.innerHTML = conteudo
 
-        const listaPrazos = document.querySelector('[data-prazos-lista]')
-        listaPrazos.appendChild(prazos)
-
+            const listaPrazos = document.querySelector('[data-prazos-lista]')
+            listaPrazos.appendChild(prazos)
+        }
+        printDatas()
     }
 }
 
 // Necessário arrumar função posteriormente
 
 botaoSubmit.addEventListener('click', registraDatas)
-botaoSubmit.addEventListener('click', printDatas)
+botaoSubmit.addEventListener('click', diffDatas)
 /* botaoSubmit.addEventListener('click', prazosList) */
